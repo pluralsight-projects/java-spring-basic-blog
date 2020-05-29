afe990b634b48016aec68728f159ff4c4b5bd5d0
Fabio91 <fabio.forcolin@gmail.com>
2020-05-29 17:04:48 +0200

    Add files via upload

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,8 +1,17 @@
 package com.pluralsight.blog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BlogController {
 
+  @GetMapping(value = "/")
+  public String listPosts(@RequestParam ModelMap modelMap){
+    modelMap.put("title", "Blog Post 1");
+    return "home";
+  }
 }
