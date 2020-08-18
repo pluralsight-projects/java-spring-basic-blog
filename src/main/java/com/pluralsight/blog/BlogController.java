366cce257675d2b80cfdb270b2197320884ae72d
jfriedman-eq <jfriedman@everquote.com>
2020-08-18 19:27:33 -0400

    First route

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,8 +1,15 @@
 package com.pluralsight.blog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.ModelMap;
 
 @Controller
 public class BlogController {
+    @RequestMapping("/")
+    public String listPosts(ModelMap m) {
+        m.put("title", "Blog Post 1");
 
+        return "home";
+    }
 }
